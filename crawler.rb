220153762498cf76c7e5810db0badda5cb0942ce
jillfarns <jillfarnsworth@yahoo.com>
2015-12-24 14:26:53 -0800

    first commit - working on front end

---
+++ crawler.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'mechanize'
+
+agent = Mechanize.new
+
+#login_form = agent.get("https://my.sa.ucsb.edu/gold/login.aspx").form('Login')
+
+#page = agent.get('https://my.sa.ucsb.edu/gold/Login.aspx?ReturnUrl=%2fgold%2f')
+
+# pp page
